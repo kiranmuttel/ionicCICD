0583298c9bfb8bda6e609e61cb1c91c46d5534e4
kiran <kiranmuttel@gmail.com>
2025-04-28 14:51:12 +0530

    new fix error staging 14

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  isProd = '';
+  constructor() {
+    this.isProd = environment.production;
+  }
 }
